cf9efce89799303a8350fcecb2d7ded2de7dc34e
Sari_nu95@itstep.edu.az <Sari_nu95@itstep.edu.az>
2021-09-28 21:33:49 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML
+{
+    class Program
+    {
+        abstract class Pizza
+        {
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public Pizza(string name, double price)
+            {
+                Name = name;
+                Price = price;
+            }
+            public abstract void getPrice();
+        }
+
+        class FrenchPizza : Pizza
+        {
+            public FrenchPizza(string name, double price) : base(name, price)
+            {
+
+            }
+
+            public override void getPrice()
+            {
+                Console.WriteLine($"Price: {Price}");
+            }
+        }
+        abstract class BaseDecorator : Pizza
+        {
+            public Pizza Pizza { get; set; }
+            public BaseDecorator(string name, double price) : base(name, price)
+            {
+
+            }
+        }
+
+        class MushroomDecorator : BaseDecorator
+        {
+            public MushroomDecorator(Pizza Pizza) : base(Pizza.Name += " Mushroom", Pizza.Price += 15)
+            {
+
+            }
+
+            public override void getPrice()
+            {
+                Console.WriteLine($"Price: {Price}");
+            }
+        }
+
+        class CheeseDecorator : BaseDecorator
+        {
+            public CheeseDecorator(Pizza Pizza) : base(Pizza.Name += " Cheese", Pizza.Price += 12)
+            {
+
+            }
+
+            public override void getPrice()
+            {
+                Console.WriteLine($"Price: {Price}");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Pizza Mypizza = new FrenchPizza("French", 5.20);
+            Mypizza = new MushroomDecorator(Mypizza);
+            Mypizza = new CheeseDecorator(Mypizza);
+            Console.Write($"Name:{Mypizza.Name}\n");
+            Mypizza.getPrice();
+        }
+    }
+}
